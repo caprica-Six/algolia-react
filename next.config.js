e8bd08e96784bccb22cd81b54b5edf1c87528cd4
pepa <mogwais@outlook.com>
2021-01-27 20:04:12 +0000

    Fix sass variables

--- next.config.js
+++ next.config.js
@@ -1,15 +1,19 @@
 /* eslint-disable import/no-commonjs */
+const path = require('path');
 
 module.exports = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
   webpack: config => {
     // Fixes npm packages that depend on `fs` module
     config.node = {
       fs: 'empty',
     };
-    config.module.rules.push({
-      test: /\.css$/,
-      loader: ['style-loader', 'css-loader'],
-    });
+    // config.module.rules.push({
+    //   test: /\.css$/,
+    //   loader: ['style-loader', 'css-loader', 'sass-loader'],
+    // });
     if (config.resolve.alias) {
       delete config.resolve.alias.react;
       delete config.resolve.alias['react-dom'];
